5c1cf6dd3c9e44686411bde7ea0d9939a1332343
hoya9802 <hoya9802@gmail.com>
2023-11-06 15:45:09 +0900

    add new algorithm

--- GraphTheory/improved_disjoint_sets.py
+++ GraphTheory/improved_disjoint_sets.py
@@ -1,7 +1,7 @@
 def find_parent(parent, x):
     if parent[x] != x:
-        return find_parent(parent, parent[x])
-    return x
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
@@ -12,9 +12,9 @@ def union_parent(parent, a, b):
         parent[a] = b
 
 v, e = map(int, input().split())
-parent = [0] * (v+1)
+parent = [0] * (v + 1)
 
-for i in range(1, v+1):
+for i in range(1, v + 1):
     parent[i] = i
 
 for i in range(e):
@@ -23,12 +23,11 @@ for i in range(e):
 
 print('각 원소가 속한 집합: ', end="")
 for i in range(1, v+1):
-    print(find_parent(parent, i), end=" ")
-
+    print(find_parent(parent, i), end= " ")
 print()
 print('부모 테이블: ', end="")
 for i in range(1, v+1):
-    print(parent[i], end=" ")
+    print(parent[i], end= " ")
 
 '''
 input:
@@ -37,8 +36,8 @@ input:
 2 3
 2 4
 5 6
-===================================
+===============
 output:
 각 원소가 속한 집합: 1 1 1 1 5 5 
-부모 테이블: 1 1 2 1 5 5 %   
+부모 테이블: 1 1 1 1 5 5 
 '''
