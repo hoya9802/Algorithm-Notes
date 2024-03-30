6b7ff6738c1e71a8193ed63b51402ded3b4a5d15
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-03-31 00:20:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.19 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.9 MB, 시간: 45.63 ms
+메모리: 39 MB, 시간: 64.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 31일 00:13:05
+2024년 03월 31일 00:20:00
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,5 +1,4 @@
 def solution(participant, completion):
-    answer = ''
     ptc = {name:0 for name in participant}
     for i in participant:
         ptc[i] += 1
@@ -8,3 +7,8 @@ def solution(participant, completion):
     for i in ptc:
         if ptc[i] != 0:
             return i
+
+import collections
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
