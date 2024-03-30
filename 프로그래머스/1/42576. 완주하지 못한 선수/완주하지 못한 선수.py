da92fece9a8d9d82264ea1062b198c37e46bf3c8
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-03-31 00:36:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.52 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,3 +1,4 @@
+# my solution
 def solution(participant, completion):
     ptc = {name:0 for name in participant}
     for i in participant:
@@ -9,6 +10,22 @@ def solution(participant, completion):
             return i
 
 import collections
+# best solution 1
 def solution(participant, completion):
     answer = collections.Counter(participant) - collections.Counter(completion)
     return list(answer.keys())[0]
+
+# best solution 2
+def solution(participant, completion):
+    answer = ''
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
+    return answer
