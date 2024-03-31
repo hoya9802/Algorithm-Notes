168d5a42d7035d8ca517f6041d102d103892e5c0
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-01 00:22:21 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,43 +1,37 @@
+# def solution(keymap, targets):
+#     answer = []
+#     key_index = {}
+#     for key in keymap:
+#         for idx, word in enumerate(key):
+#             if word not in key_index:
+#                 key_index[word] = idx+1
+#             else:
+#                 key_index[word] = min(key_index[word], idx+1)
+#     for target in targets:
+#         temp = 0
+#         for word in target:
+#             if word in key_index:
+#                 temp += key_index[word]
+#             else:
+#                 temp = -1
+#                 break
+#         answer.append(temp)
+#     return answer
+
 def solution(keymap, targets):
     answer = []
-    key_index = {}
-    for key in keymap:
-        for idx, word in enumerate(key):
-            if word not in key_index:
-                key_index[word] = idx+1
-            else:
-                key_index[word] = min(key_index[word], idx+1)
-    for target in targets:
-        temp = 0
-        for word in target:
-            if word in key_index:
-                temp += key_index[word]
-            else:
-                temp = 0
+    hs = {}
+    for k in keymap:
+        for i, ch in enumerate(k):
+            hs[ch] = min(i + 1, hs[ch]) if ch in hs else i + 1
+
+    for i, t in enumerate(targets):
+        ret = 0
+        for ch in t:
+            if ch not in hs:
+                ret = - 1
                 break
-        if temp == 0:
-            answer.append(-1)
-        else:
-            answer.append(temp)
+            ret += hs[ch]
+        answer.append(ret)
+
     return answer
-
-# def solution(keymap, targets):
-#     keytable = {}
-#     for keys in keymap:
-#         for i, key in enumerate(keys):
-#             if key not in keytable:
-#                 keytable[key] = i + 1
-#             else:
-#                 keytable[key] = min(keytable[key], i + 1)
-
-#     result = []
-#     for target in targets:
-#         clicked = 0
-#         for key in target:
-#             if key not in keytable:
-#                 clicked = -1
-#                 break
-#             clicked += keytable[key]
-#         result.append(clicked)
-
-#     return result
