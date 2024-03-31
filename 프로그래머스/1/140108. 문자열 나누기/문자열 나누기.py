75d4a28bde69c7e40c64c2989f7d743d4b9dcfe5
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-03-31 23:33:57 +0900

    [level 1] Title: 문자열 나누기, Time: 0.82 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,15 +1,13 @@
-from collections import defaultdict
-
 def solution(s):
     answer = 0
-    cnt1=0; cnt2=0
-    for i in s:
-        if cnt1==cnt2:
-            answer+=1
-            k=i
-        if k==i:
-            cnt1+=1
+    cnt_1, cnt_2 = 0, 0
+    for word in s:
+        if cnt_1 == cnt_2:
+            answer += 1
+            k = word
+        if k == word:
+            cnt_1 += 1
         else:
-            cnt2+=1
-
+            cnt_2 += 1
+        
     return answer
