f00457d13e9b81e1d27304183291203089be67a7
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-02 00:58:44 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -0,0 +1,15 @@
+def solution(N, stages):
+    answer = []
+    for n in range(1, N+1):
+        n_stage = stages.count(n)
+        if n_stage != 0:
+            answer.append(n_stage/len(stages))
+        else:
+            answer.append(0)
+        if n in stages:
+            stages = [x for x in stages if x != n]
+    indexed_list = [(value, index) for index, value in enumerate(answer)]
+    sorted_list = sorted(indexed_list, key=lambda x: (-x[0], x[1]))
+    res = [index+1 for value, index in sorted_list]
+    
+    return res
