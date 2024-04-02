b1a4d89b57a1e027a2aa3acc7db281e98999ec0b
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-03 00:59:29 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,9 @@
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse=True)
+    for i in range(0,len(score),m):
+        if i + m <= len(score):
+            answer += score[i+m-1]
+            
+    answer *= m
+    return answer
