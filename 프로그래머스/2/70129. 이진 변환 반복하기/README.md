6498677827b078aec81a9450879b98e3aa82413d
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-04 21:40:54 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.39 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 2.29 ms
+메모리: 10.1 MB, 시간: 0.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 04일 21:36:39
+2024년 04월 04일 21:40:53
 
 ### 문제 설명
 
--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,15 +1,7 @@
-from collections import Counter
-
 def solution(s):
     answer = [0,0]
-    
-    while 1:    
+    while s != '1':
         answer[0] += 1
-        if s.count('0') != 0:
-            answer[1] += s.count('0')
-        s = s.replace('0', '')
-        if len(s) == 1:
-            return answer
-        s = bin(len(s))[2:]
-        
+        answer[1] += len(s) - s.count('1')
+        s = bin(s.count('1'))[2:]
     return answer
