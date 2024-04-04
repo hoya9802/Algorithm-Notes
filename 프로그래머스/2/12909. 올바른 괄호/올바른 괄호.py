0bd1994608a78631f30d1b904a7a4bf8db5dba00
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-04 20:44:23 +0900

    [level 2] Title: 올바른 괄호, Time: 9.19 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def solution(s):
+    answer = True
+    stack = deque()
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        else:
+            if len(stack) == 0 or stack.pop() != '(':
+                return False
+    if len(stack) > 0:
+        return False
+    return True
+            
+    
+
+    return True
