bbfd167fbd8319c8a8bb2fdee712bb770c3e7b2f
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-05 02:21:38 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,10 +1,7 @@
 import math
 
 def solution(brown, yellow):
-    answer = []
-    n = brown+yellow
-    for i in range(1, int(math.sqrt(n))+1):
-        if n % i == 0:
-            if (n//i - 2)*(i-2) == yellow:
-                answer.append([n//i, i])
-    return answer[-1]
+    for i in range(1, int(math.sqrt(yellow))+1):
+        if yellow % i == 0:
+            if 2*(i+(yellow//i)) == brown-4:
+                return [yellow/i+2,i+2]
