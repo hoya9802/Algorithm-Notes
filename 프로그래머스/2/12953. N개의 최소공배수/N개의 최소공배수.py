97b4920df574cff88a95099bfcf5efab5c7bfd12
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-07 14:24:55 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -1,28 +1,14 @@
-from collections import deque
-
 def find_lcm(x, y):
-    ans = 1
-    max_val = max(x,y); min_val = min(x,y)
-    stack = list();
-    i = 2  
-    while min_val >= i:
-        if max_val % i == 0 and min_val % i == 0:
-            max_val //= i; min_val //= i
-            stack.append(i)
-        else:
-            i += 1 
-
-    stack.append(max_val); stack.append(min_val)
-    
-    for i in stack:
-        ans *= i
-    return ans
+    if y == 0:
+        return x
+    else:
+        return find_lcm(y, x%y)
         
 def solution(arr):
     ans = arr[0]
     
     for i in arr[1:]:
-        t = find_lcm(ans, i)
+        t = ans * i // find_lcm(ans, i)
         ans = t
     
     return ans
