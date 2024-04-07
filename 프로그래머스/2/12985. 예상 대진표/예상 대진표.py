12d013940e0bd554ac87c16f479477d766247485
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-07 16:49:12 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -3,12 +3,7 @@ import math
 def solution(n,a,b):
     i = 1
     while i <= int(math.log(n,2)):
-        if a % 2 != 0:
-            a += 1
-        if b % 2 != 0:
-            b += 1
+        a, b = (a+1)//2, (b+1)//2
         if a == b:
             return i
-        else:
-            a //= 2; b //= 2
         i += 1
