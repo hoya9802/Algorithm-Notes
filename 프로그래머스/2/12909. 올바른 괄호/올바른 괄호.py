6174818e75861910843310eb733a0c9087548b7f
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-09 00:47:29 +0900

    [level 2] Title: 올바른 괄호, Time: 10.61 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,15 +1,12 @@
-# solve 2
-from collections import deque
-
 def solution(s):
-    answer = True
-    stack = deque()
+    stack = []
     for i in s:
         if i == '(':
             stack.append(i)
         else:
             if len(stack) == 0 or stack.pop() != '(':
                 return False
-    if len(stack) != 0:
+    if len(stack) == 0:
+        return True
+    else:
         return False
-    return True
