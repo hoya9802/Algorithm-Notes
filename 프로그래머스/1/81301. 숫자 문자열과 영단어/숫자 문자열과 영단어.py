6ec8cf725627f12fe4bfef362e7a5be4451056d7
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-09 01:03:44 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,7 +1,8 @@
+# solve 2
 def solution(s):
-    num = ['zero','one','two','three','four','five','six','seven','eight','nine']
-    for n in num:
+    number_name = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+    for n in number_name:
         if n in s:
-            s = s.replace(n, str(num.index(n)))
-    
+            s = s.replace(n, str(number_name[n]))
+
     return int(s)
