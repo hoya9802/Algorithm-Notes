6738230212924be45e86b41a2aa68e03c9836d23
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-09 15:34:41 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.02 ms
+메모리: 10.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 09일 15:26:42
+2024년 04월 09일 15:34:40
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,19 +1,11 @@
-from collections import defaultdict
+from collections import Counter
 
 def solution(want, number, discount):
     cnt = 0
     for i in range(0, len(discount)-9):
-        want_dict = defaultdict(int, {name:n for name, n in zip(want, number)})
-        tmp = discount[i:10+i]
-        for n in tmp:
-            if n in want_dict.keys():
-                want_dict[n] -= 1
-        check = True
-        for c in want_dict.values():
-            if c > 0:
-                check = False
-                break
-        if check:
+        want_count = Counter({name:n for name, n in zip(want, number)})
+        tmp_count = Counter(discount[i:10+i])
+        if want_count == tmp_count:
             cnt += 1
-    
+        
     return cnt
