e67c3b571deb49fd118cda0de9232122c8719190
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-10 01:45:38 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -5,8 +5,8 @@ def solution(clothes):
         clodict[i[-1]]=0
     
     for k in clothes:
-        clodict[k[-1]]+=len(k[:-1])
-    print(clodict)
+        clodict[k[-1]]+=1
+        
     for h in clodict.values():
         ans=(h+1)*ans
         
