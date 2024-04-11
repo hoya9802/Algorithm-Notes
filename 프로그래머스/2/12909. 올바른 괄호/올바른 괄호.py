57ec02c7fd48abb6ad10e119b05b53fd134b978b
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-11 22:46:13 +0900

    [level 2] Title: 올바른 괄호, Time: 9.49 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,14 @@
-# solve 2
+# solve 3
+
 def solution(s):
     answer = True
     stack = []
     for i in s:
-        if i == '(':
+        if i == "(":
             stack.append(i)
         else:
             if len(stack) == 0 or stack.pop() != '(':
                 return False
     if len(stack) != 0:
         return False
-
     return True
