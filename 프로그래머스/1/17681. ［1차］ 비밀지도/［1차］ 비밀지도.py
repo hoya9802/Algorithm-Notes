1ef8e16d482d7459b66ccf45bd84d708253ece3b
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-15 03:19:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,7 +1,7 @@
+# solve 1
+
 def solution(n, arr1, arr2):
     answer = []
-    for i, j in zip(arr1, arr2):
-        a = bin(i|j)[2:].zfill(n).replace('1', '#').replace('0', ' ')
-        answer.append(a)
-
+    for a1, a2 in zip(arr1, arr2):
+        answer.append(bin(a1|a2)[2:].zfill(n).replace('1', '#').replace('0', ' '))
     return answer
