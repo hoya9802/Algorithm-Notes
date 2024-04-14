70a982e9fae5e36e7af1cf8859d35ddf89d80a14
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-15 03:07:23 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,8 +1,7 @@
-# solve 2
-def solution(s):
-    number_name = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
-    for n in number_name:
-        if n in s:
-            s = s.replace(n, str(number_name[n]))
+# solve 3
 
+def solution(s):
+    name_number = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+    for i in name_number:
+        s = s.replace(i, str(name_number[i]))
     return int(s)
