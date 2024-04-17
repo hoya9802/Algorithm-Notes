4949c183300c0e392b72bfda45b0db7bc1d21935
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-17 21:29:40 +0900

    [level 2] Title: 모음 사전, Time: 1.29 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -3,9 +3,9 @@ from itertools import product
 def solution(word):
     answer = []
     for i in range(1,6):
-        ls = list(product(["A","E","I",'O','U'], repeat=i))
-        for i in ls:
-            answer.append(''.join(i))
+        tmp = list(product(['A','E','I','O','U'], repeat=i))
+        for j in tmp:
+            answer.append(''.join(j))
     answer.sort()
     res = answer.index(word) + 1
         
