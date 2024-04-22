c6c8f1a280be6567028b13e5d315e38ceca48417
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-22 22:50:26 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,10 +1,9 @@
-# solve 3
+# solve 4
 from collections import Counter
 
 def solution(clothes):
-    answer = Counter([x for cl, x in clothes])
-    temp = 1
-    for i in answer.values():
-        temp *= (i+1)
-        
-    return temp - 1
+    answer = 1
+    cnt = Counter([x for n, x in clothes])
+    for i in cnt.values():
+        answer *= (i + 1)
+    return answer - 1
