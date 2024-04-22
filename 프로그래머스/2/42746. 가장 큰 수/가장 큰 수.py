8d9cb8d2653644cc70e427d2a011ef46006a6a9f
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-04-22 22:55:55 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,18 +1,17 @@
+# solve 1
 from functools import cmp_to_key
 
 def compare(x, y):
-    if x+y>=y+x:
+    if x + y >= y + x:
         return -1
     else:
         return 1
-    
 
 def solution(numbers):
     numbers = list(map(str, numbers))
     numbers.sort(key=cmp_to_key(compare))
     res = ''.join(numbers)
-    print(len(res))
-    if res == '0'*len(res):
+    if res == '0' * len(res):
         return '0'
     else:
         return res
