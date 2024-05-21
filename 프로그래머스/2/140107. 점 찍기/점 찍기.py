e2b7053d778889cc545632f823d97828abdb4c4c
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-05-22 00:21:57 +0900

    [level 2] Title: 점 찍기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.py
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.py
@@ -3,5 +3,5 @@ import math
 def solution(k, d):
     ans = 0
     for i in range(d//k+1):
-        ans += (math.sqrt(d**2-(k*i)**2))//k+1
+        ans += ((d**2-(k*i)**2)**(1/2))//k+1
     return ans
