23e70640b1392297b2baaf675a98b3141431c0eb
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-05-24 18:48:11 +0900

    [level 2] Title: N-Queen, Time: 1583.55 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,20 +1,20 @@
+# solve 1
+
 def solution(n):
     global ans
     ans = 0
     v1 = [0] * n
     v2 = [0] * (2*n)
     v3 = [0] * (2*n)
-    
     def dfs(start):
         global ans
         if start == n:
             ans += 1
-            return 
+            return
         for i in range(n):
-            if v1[i] == v2[i+start] == v3[start-i] == 0:
-                v1[i] = v2[i+start] = v3[start-i] = 1
+            if v1[i] == v2[start+i] == v3[start-i] == 0:
+                v1[i] = v2[start+i] = v3[start-i] = 1
                 dfs(start+1)
-                v1[i] = v2[i+start] = v3[start-i] = 0
-
+                v1[i] = v2[start+i] = v3[start-i] = 0
     dfs(0)
     return ans
