de54b028500222c361240b6820c9cb20dd720c10
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-05-27 23:50:44 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
@@ -1,11 +1,13 @@
+# solve 1
 def check_win(board, ch):
     for i in range(3):
         if all(ch==x for x in board[i]):
             return True
+        
     for i in range(3):
         if all(ch==board[x][i] for x in range(3)):
             return True
-    
+        
     if all(ch==board[x][x] for x in range(3)):
         return True
     
@@ -14,12 +16,13 @@ def check_win(board, ch):
     return False
 
 def solution(board):
-    oc, xc = 0, 0
-    for i in board:
-        oc += i.count('O'); xc += i.count('X')
-    if oc < xc or oc > xc + 1:
+    oc = xc = 0
+    for b in board:
+        oc += b.count('O'); xc += b.count('X')
+    if oc < xc or oc > xc+1:
         return 0
-
+    
     if (check_win(board, 'O') and oc != xc + 1) or (check_win(board, 'X') and oc != xc):
         return 0
+    
     return 1
