c427ef9f0b3d47d3ba49133c832ae80cabc56b48
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-05-28 21:51:55 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,26 +1,13 @@
-def count_seq(x):
-    return min((ord('Z')+1 - ord(x)), (ord(x) - ord('A')))
+def check_vert(x):
+    return min((ord('Z')+1) - ord(x), ord(x) - ord('A'))
 
 def solution(name):
-    # 알파벳 변경 횟수( 상하 이동 ) 
-    spell_move = 0
-    # 커서 이동 횟수, 이름의 길이 - 1( 좌우 이동 )
-    cursor_move = len(name) - 1  
-    for i, spell in enumerate(name):
-    	# 알파벳 변경 횟수, 위아래 중 최소 이동 값 ( 상하 이동 )
-        spell_move += count_seq(spell)
-        # 해당 알파벳 다음부터 연속된 A 문자열 찾기
-        next = i + 1
-        while next < len(name) and name[next] == 'A':
-            next += 1
-            
-        # 아래 3가지 경우 중 최소 이동 값으로 갱신
-        # 1. 이전 커서 이동 값 ( 초기값 - 이름의 길이 - 1 )
-        # 2. 연속된 A의 왼쪽 시작
-        # 3. 연속된 A의 오른쪽 시작
-        temp = [cursor_move, 2 * i + len(name) - next, i + 2 * (len(name) - next)]
-
-        cursor_move = min(temp)    
-        
-    # 조이스틱 조작 횟수 = 알파벳 변경 횟수( 상하 이동 ) + 커서 이동 횟수( 좌우 이동 )    
-    return spell_move + cursor_move
+    res = 0
+    hori_min_move = len(name) - 1
+    for idx, val in enumerate(name):
+        res += check_vert(val)
+        nxt = idx + 1
+        while nxt < len(name) and name[nxt] == 'A':
+            nxt += 1
+        hori_min_move = min(hori_min_move, idx*2 + len(name)-nxt, idx + 2*(len(name)-nxt))
+    return res + hori_min_move
