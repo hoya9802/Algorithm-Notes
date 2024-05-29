0bb25ca08faf3e6bef273f31e15a552a2a2b56dd
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-05-29 12:45:04 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.py
@@ -1,28 +1,23 @@
-# solve 1
+# solve 2
 def check_win(board, ch):
     for i in range(3):
-        if all(ch==x for x in board[i]):
+        if all(x == ch for x in board[i]):
             return True
-        
     for i in range(3):
-        if all(ch==board[x][i] for x in range(3)):
+        if all(ch == board[x][i] for x in range(3)):
             return True
-        
-    if all(ch==board[x][x] for x in range(3)):
+    if all(ch == board[x][x] for x in range(3)):
         return True
-    
-    if all(ch==board[2-x][x] for x in range(3)):
+    if all(ch == board[2-x][x] for x in range(3)):
         return True
     return False
 
 def solution(board):
-    oc = xc = 0
-    for b in board:
-        oc += b.count('O'); xc += b.count('X')
-    if oc < xc or oc > xc+1:
+    xc = oc = 0
+    for i in board:
+        xc += i.count('X'); oc += i.count('O')
+    if oc < xc or oc > xc + 1:
         return 0
-    
-    if (check_win(board, 'O') and oc != xc + 1) or (check_win(board, 'X') and oc != xc):
+    if (check_win(board,'O') and oc != xc + 1) or (check_win(board, 'X') and oc != xc):
         return 0
-    
     return 1
