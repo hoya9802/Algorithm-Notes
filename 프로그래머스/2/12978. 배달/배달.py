1d7a6fe8b470d7dd9a15a25a5ed23c94a6f4e77c
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-05-31 20:52:31 +0900

    [level 2] Title: 배달, Time: 0.07 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -1,17 +1,27 @@
-# solve 6
+# solve 7
+import heapq
 
 def solution(N, road, K):
     INF = int(1e9)
-    graph = [[INF] * (N+1) for _ in range(N+1)]
+    graph = [[] for _ in range(N+1)]
+    distance = [INF] * (N+1)
+    
     for r in road:
         a, b, c = r
-        graph[a][b] = min(graph[a][b],c); graph[b][a] = min(graph[b][a],c)
-    for i in range(N+1):
-        for j in range(N+1):
-            if i == j:
-                graph[i][j] = 0
-    for k in range(1, N+1):
-        for x in range(1, N+1):
-            for y in range(1, N+1):
-                graph[x][y] = min(graph[x][k] + graph[k][y], graph[x][y])
-    return sum(1 for x in graph[1] if x <= K)
+        graph[a].append((b,c)); graph[b].append((a,c))
+    
+    def dijkstra(start):
+        q = []
+        distance[start] = 0
+        heapq.heappush(q, (0, start))
+        while q:
+            dist, now = heapq.heappop(q)
+            if distance[now] < dist:
+                continue
+            for i in graph[now]:
+                cost = dist + i[1]
+                if cost < distance[i[0]]:
+                    distance[i[0]] = cost
+                    heapq.heappush(q, (cost, i[0]))
+    dijkstra(1)
+    return sum(1 for x in distance[1:] if x <= K)
