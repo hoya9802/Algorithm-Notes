5ab10e898eca61faf9a6420fe0f5936b601714f6
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-06-04 17:43:41 +0900

    [Silver V] Title: 분수찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,15 +1,14 @@
 import sys
-n = int(sys.stdin.readline())
-line = 0
-end = 0
-while n > end:
+input = sys.stdin.readline
+
+x = int(input())
+line = 1
+while x > line:
+    x -= line
     line += 1
-    end += line
-diff = end - n
 if line % 2 == 0:
-    t = line - diff
-    b = diff + 1
+    t = x; b = line -(x-1)
 else:
-    t = diff + 1
-    b = line - diff
-print(f"{t}/{b}")
+    t = line-(x-1)
+    b = x
+print(f'{t}/{b}')
