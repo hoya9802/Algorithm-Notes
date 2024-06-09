8dccbd5bfcab2127e3698afa5ce1295bb3e56686
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-06-09 22:57:57 +0900

    [level 2] Title: 전화번호 목록, Time: 84.57 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,8 @@
-# solve 1
+# solve 4
 
 def solution(phone_book):
-    answer = True
     phone_book.sort()
     for i in range(len(phone_book)-1):
-        if phone_book[i+1].startswith(phone_book[i]) == True:
+        if phone_book[i+1].startswith(phone_book[i]):
             return False
-    return answer
+    return True
