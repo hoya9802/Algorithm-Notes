eb7424a3c626f3b1daec663e30426344ad74633b
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-06-10 23:19:10 +0900

    [level 2] Title: 타겟 넘버, Time: 4.48 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,15 @@
-answer = 0
-def dfs(i, total, numbers, target):
-    global answer
-    if i == len(numbers):
-        if total == target:
-            answer += 1
-        return
-    dfs(i+1, total+numbers[i], numbers, target)
-    dfs(i+1, total-numbers[i], numbers, target)
-    
 def solution(numbers, target):
-    global answer
-    dfs(0, 0, numbers, target)
-    return answer
+    ans = 0
+
+    def dfs(sm, idx):
+        nonlocal ans
+        if idx == len(numbers):
+            if sm == target:
+                ans += 1
+            return
+        else:
+            dfs(sm + numbers[idx], idx + 1)
+            dfs(sm - numbers[idx], idx + 1)
+
+    dfs(0, 0)
+    return ans
