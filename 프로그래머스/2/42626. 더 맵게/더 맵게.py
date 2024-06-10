3c09da7b6d4ee65f13dfd22d70e9fcc138713660
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-06-11 01:34:18 +0900

    [level 2] Title: 더 맵게, Time: 1689.86 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,18 +1,17 @@
-import heapq
+# solve 1
 
-def mix_sco(a, b):
-    return a + (b * 2)
+import heapq
+def mixed_sco(x, y):
+    return x + (y*2)
 
 def solution(scoville, K):
-    answer = 0
     heapq.heapify(scoville)
-    while len(scoville) >= 2 and scoville[0] < K:
-        a = heapq.heappop(scoville)
-        b = heapq.heappop(scoville)
-        tmp = mix_sco(a, b)
-        heapq.heappush(scoville, tmp)
-        answer += 1
-    if len(scoville) == 1 and scoville[0] < K:
+    res = 0
+    while len(scoville)>=2 and scoville[0] < K:
+        res += 1
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        heapq.heappush(scoville, mixed_sco(first, second))
+    if scoville[0] < K:
         return -1
-        
-    return answer
+    return res
