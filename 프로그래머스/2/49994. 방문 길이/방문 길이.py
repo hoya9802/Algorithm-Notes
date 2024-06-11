f6db91dcbe3613bd894b36c7380689aaebc5a7df
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-06-11 21:48:45 +0900

    [level 2] Title: 방문 길이, Time: 3.79 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,16 +1,14 @@
 def solution(dirs):
-    answer = 0
-    x, y = 5,5
-    direction = ['U','D','R','L']
-    movements = [(-1,0), (1,0), (0,1), (0,-1)]
-    direct = []
-    for i in dirs:
-        nx = x + movements[direction.index(i)][0]
-        ny = y + movements[direction.index(i)][1]
-        if nx < 0 or ny < 0 or nx > 10 or ny > 10:
+    hist = []
+    x, y = 5, 5;
+    cmd = ['U', 'D', 'R', 'L']
+    movement = [(-1,0), (1,0), (0,1), (0,-1)]
+    for d in dirs:
+        nx = x + movement[cmd.index(d)][0]; ny = y + movement[cmd.index(d)][1]
+        if nx < 0 or nx > 10 or ny < 0 or ny > 10:
             continue
-        if [x,y,nx,ny] not in direct and [nx,ny,x,y] not in direct:
-            direct.append([x,y,nx,ny])
+        if [(x,y),(nx,ny)] not in hist and [(nx,ny),(x,y)] not in hist:
+            hist.append([(x,y),(nx,ny)])
         x, y = nx, ny
-    
-    return len(direct)
+    return len(hist)
+        
