4bb3656fb42b0250b7630d8b34fd511f1b2334aa
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-06-16 13:34:24 +0900

    [level 2] Title: 하노이의 탑, Time: 9.56 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,13 +1,11 @@
-# solve 4
-
 def solution(n):
-    ans = []
+    res = []
     def hanoi(n, start, target, sub):
         if n == 1:
-            ans.append([start, target])
+            res.append([start, target])
             return
         hanoi(n-1, start, sub, target)
-        ans.append([start, target])
+        res.append([start, target])
         hanoi(n-1, sub, target, start)
     hanoi(n, 1, 3, 2)
-    return ans
+    return res
