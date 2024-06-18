c16f7d427ac3bc936894a8d25510fa9c6674a62a
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-06-19 00:22:49 +0900

    [Silver III] Title: 1로 만들기, Time: 548 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -2,14 +2,11 @@ import sys
 input = sys.stdin.readline
 
 x = int(input())
-INF = int(1e9)
-dp = [INF] * (x+1)
-dp[1] = 0
+dp = [0] * (x+1)
 for i in range(2, x+1):
+    dp[i] = dp[i-1] + 1
     if i % 3 == 0:
         dp[i] = min(dp[i], dp[i//3]+1)
     if i % 2 == 0:
         dp[i] = min(dp[i], dp[i//2]+1)
-    if i - 1 != 0:
-        dp[i] = min(dp[i], dp[i-1]+1)
 print(dp[x])
