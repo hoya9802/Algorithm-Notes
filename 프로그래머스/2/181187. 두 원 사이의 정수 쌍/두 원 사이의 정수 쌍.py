b13463c34f7c00ae3ea2eaf717dafbac889bbbd8
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-06-18 18:43:57 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 822.32 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.py
@@ -1,10 +1,10 @@
 import math
 
 def solution(r1, r2):
-    ans = 0
+    res = 0
     for i in range(1, r2+1):
         if i < r1:
-            ans += (math.floor(math.sqrt(r2**2-i**2))-math.ceil(math.sqrt(r1**2-i**2))+1)
+            res += (math.floor(math.sqrt(r2**2-i**2)) - math.ceil(math.sqrt(r1**2-i**2))+1)
         else:
-            ans += (math.floor(math.sqrt(r2**2-i**2)) + 1)
-    return ans * 4
+            res += (math.floor(math.sqrt(r2**2-i**2))+1)
+    return res * 4
