b6d9566416894aae123c3bd8cbf1c53c929102bf
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-06-18 22:06:53 +0900

    [level 2] Title: N-Queen, Time: 1631.72 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,20 +1,18 @@
-# solve 3
-
 def solution(n):
     ans = 0
     v1 = [0] * n
     v2 = [0] * (2*n)
     v3 = [0] * (2*n)
     
-    def dfs(start):
+    def dfs(v):
         nonlocal ans
-        if start == n:
+        if v == n:
             ans += 1
             return
         for i in range(n):
-            if v1[i] == v2[i-start] == v3[i+start] == 0:
-                v1[i] = v2[i-start] = v3[i+start] = 1
-                dfs(start+1)
-                v1[i] = v2[i-start] = v3[i+start] = 0
+            if v1[i] == v2[v-i] == v3[v+i] == 0:
+                v1[i] = v2[v-i] = v3[v+i] = 1
+                dfs(v+1)
+                v1[i] = v2[v-i] = v3[v+i] = 0
     dfs(0)
     return ans
