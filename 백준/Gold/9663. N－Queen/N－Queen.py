0b39b5e71d5254ca946927d21a6046a01aa5f5a8
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-07-01 13:47:39 +0900

    [Gold IV] Title: N-Queen, Time: 6424 ms, Memory: 120848 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -6,16 +6,17 @@ v1 = [0] * n
 v2 = [0] * (2*n)
 v3 = [0] * (2*n)
 
-res = 0
-def n_queen(i):
-    global res
-    if i == n:
-        res += 1
+cnt = 0
+def dfs(v):
+    global cnt
+    if v == n:
+        cnt += 1
         return
-    for j in range(n):
-        if v1[j] == v2[i+j] == v3[i-j] == 0:
-            v1[j] = v2[i+j] = v3[i-j] = 1
-            n_queen(i+1)
-            v1[j] = v2[i+j] = v3[i-j] = 0
-n_queen(0)
-print(res)
+    for i in range(n):
+        if v1[i] == v2[i+v] == v3[i-v] == 0:
+            v1[i] = v2[i+v] = v3[i-v] = 1
+            dfs(v+1)
+            v1[i] = v2[i+v] = v3[i-v] = 0
+
+dfs(0)
+print(cnt)
