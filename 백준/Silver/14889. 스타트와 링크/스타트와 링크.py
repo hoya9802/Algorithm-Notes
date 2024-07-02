a7b8a85b989409b94746bfac92ef472dad5ac6e9
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-07-02 17:43:22 +0900

    [Silver I] Title: 스타트와 링크, Time: 2704 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+a_list = []; b_list = []
+
+def cal(x, y):
+    x_val, y_val = 0, 0
+    for i in range(len(x)):
+        for j in range(i+1,len(x)):
+            x_val += (graph[x[i]][x[j]] + graph[x[j]][x[i]])
+    for i in range(len(y)):
+        for j in range(i+1,len(y)):
+            y_val += (graph[y[i]][y[j]] + graph[y[j]][y[i]])
+    return abs(x_val - y_val)
+
+ans = int(1e9)
+def dfs(n, a_list, b_list):
+    global ans
+    if n == N:
+        if len(a_list) == len(b_list):
+            ans = min(ans, cal(a_list, b_list))
+        return
+    
+    dfs(n+1, a_list+[n], b_list)
+    dfs(n+1, a_list, b_list+[n])
+
+dfs(0, a_list, b_list)
+print(ans)
