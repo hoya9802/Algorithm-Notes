f43e2bfd1d1f3e2cbb266695f237904b58617e4b
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-07-03 14:14:45 +0900

    [Gold V] Title: 암호 만들기, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+L, C = map(int, input().split())
+data = sorted(list(input().split()))
+
+def check(x):
+    cnt = 0
+    for i in x:
+        if i in ['a','i','o','u','e']:
+            cnt += 1
+    if 1 <= cnt <= L-2:
+        return True
+    return False
+
+def dfs(n, s):
+    if n == C:
+        if check(s) and len(s) == L:
+            print(''.join(s))
+        return
+    dfs(n+1, s+[data[n]])
+    dfs(n+1, s)
+
+dfs(0,[])
