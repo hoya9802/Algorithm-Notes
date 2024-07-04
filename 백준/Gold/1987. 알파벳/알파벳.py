41f5f1f8f3288d4412ada283e7e057eff84ccf75
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-07-04 15:18:45 +0900

    [Gold IV] Title: 알파벳, Time: 1572 ms, Memory: 161184 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,24 +1,27 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 r, c = map(int, input().split())
 graph = [list(input()) for _ in range(r)]
-visited = [0] * 100
 movement = [(-1,0), (0,1), (1,0), (0,-1)]
-visited[ord(graph[0][0])] = 1
-ans = 1
 
-def dfs(x,y,cnt):
-    global ans
-    ans = max(ans, cnt)
+def bfs():
+    ans = 1
+    q = deque([(0,0,graph[0][0])])
+    visited = [[set() for _ in range(c)] for _ in range(r)]
+    visited[0][0].add(graph[0][0])
+    while q:
+        x, y, v = q.popleft()
+        ans = max(len(v), ans)
+        for i in range(4):
+            nx = x + movement[i][0]; ny = y + movement[i][1]
+            if nx < 0 or nx >= r or ny < 0 or ny >= c or graph[nx][ny] in v:
+                continue
+            if v + graph[nx][ny] in visited[nx][ny]:
+                continue
+            q.append((nx,ny,v+graph[nx][ny]))
+            visited[nx][ny].add((v+graph[nx][ny]))
+    return ans
 
-    for i in range(4):
-        nx = x + movement[i][0]; ny = y + movement[i][1]
-        if nx < 0 or nx >= r or ny < 0 or ny >= c or visited[ord(graph[nx][ny])] == 1:
-            continue
-        visited[ord(graph[nx][ny])] = 1
-        dfs(nx,ny,cnt+1)
-        visited[ord(graph[nx][ny])] = 0
-    return
-dfs(0,0,1)
-print(ans)
+print(bfs())
