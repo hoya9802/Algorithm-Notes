60fdc2dbf99b90a1d3fbc72926b24840ebb0c225
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-07-10 11:47:15 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 55044 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -6,6 +6,5 @@ lst = deque([x for x in range(1, int(input())+1)])
 
 while len(lst) != 1:
     lst.popleft()
-    back = lst.popleft()
-    lst.append(back)
+    lst.append(lst.popleft())
 print(lst[0])
