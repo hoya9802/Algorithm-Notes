66d610ef4fd386c5433b24b8a722c5b6b4b47d7f
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-07-15 23:48:35 +0900

    [Silver IV] Title: 상근이의 여행, Time: 228 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    graph = [[] for _ in range(n+1)]
+    for _ in range(m):
+        a, b = map(int, input().split())
+        graph[a].append(b); graph[b].append(a)
+    res = 0
+    visited = [False] * (n+1)
+    def dfs(v):
+        global res
+        stack = deque([v])
+        while stack:
+            now = stack.pop()
+            if not visited[now]:
+                visited[now] = True
+                res += 1
+                for i in graph[now]:
+                    stack.append(i)
+    dfs(1)
+    print(res-1)
