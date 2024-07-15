241971b9386db3247fecf02e3310f12f1a6ab6d0
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-07-15 14:29:04 +0900

    [Gold IV] Title: 부분합, Time: 108 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,20 +1,25 @@
 import sys
-n, m = map(int, input().split())
-fsum = [0] + list(map(int, input().split()))
-for i in range(1,n+1):
-    fsum[i] += fsum[i-1]
+input = sys.stdin.readline
 
-pointer = 0; res = 100000
-for i in range(1,n+1):
-    if fsum[i] < m:
+n, s = map(int, input().split())
+data = [0]+list(map(int, input().split()))
+
+for i in range(1,len(data)):
+    data[i] += data[i-1]
+
+pointer = 0; res = 100_000
+for i in range(len(data)):
+    if data[i] < s:
         continue
     while pointer < i:
-        if fsum[i] - fsum[pointer] >= m:
-            if i - pointer < res:
+        temp = data[i] - data[pointer]
+        if temp >= s:
+            if res > i - pointer:
                 res = i - pointer
-            pointer += 1
         else:
             break
-if res == 100000:
+        pointer += 1
+if res == 100_000:
     print(0)
-else: print(res)
+else:
+    print(res)
