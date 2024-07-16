dc886e3cb99c9965d31562c15de35c1a8e20a1e5
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-07-16 21:29:02 +0900

    [Silver III] Title: N과 M (1), Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -3,14 +3,14 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-lst = []
-def dfs():
-    if len(lst) == m:
-        print(' '.join(map(str, lst)))
+def dfs(s):
+    if len(s) == m:
+        print(' '.join(map(str,s)))
         return
-    for i in range(1,n+1):
-        if i not in lst:
-            lst.append(i)
-            dfs()
-            lst.pop()
-dfs()
+    for i in range(1, n+1):
+        if i not in s:
+            s.append(i)
+            dfs(s)
+            s.pop()
+
+dfs([])
