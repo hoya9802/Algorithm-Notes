293ca2fb3a08eaad6ad50406e1bdccad4d165038
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-07-18 18:36:55 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 96 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,35 +1,34 @@
-import sys
-import heapq
+import sys, heapq
 input = sys.stdin.readline
-N = int(input())
 
-dx=[-1,0,1,0]
-dy=[0,-1,0,1]
-cnt = 0
+INF = int(1e9)
+movement = [(-1,0), (0,1), (1,0), (0,-1)]
+i = 1
+while 1:
+    n = int(input())
+    if n == 0:
+        break
+    graph = []
+    distances = [[INF] * n for _ in range(n)]
+    for _ in range(n):
+        graph.append(list(map(int, input().split())))
 
-# ✨ 문제 조건대로 진행
-while N!=0:
-    cnt+=1
-    board = [list(map(int,input().split())) for _ in range(N)]
-    ch_board = [[-1]*N for _ in range(N)]
+    def dijkstra(x,y):
+        q = []
+        distances[x][y] = graph[x][y]
+        heapq.heappush(q, (graph[x][y], x, y))
 
-    hq = []
-    heapq.heappush(hq,(board[0][0],0,0))
-    ch_board[0][0] = 0
-    
-# ✨ dijkstra
-    while hq:
-        val,x,y = heapq.heappop(hq)
-        # ✨ 출력
-        if x == N-1 and y == N-1:
-            print(f'Problem {cnt}: {val}')
-            break
-        # ✨ 상하좌우 확인
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0<=nx < N and 0<=ny<N and ch_board[nx][ny] == -1:
-                ch_board[nx][ny] = 0
-                heapq.heappush(hq,(val+board[nx][ny],nx,ny))
-
-    N = int(input())
+        while q:
+            dist, x, y = heapq.heappop(q)
+            if x == n-1 and y == n-1:
+                break
+            if distances[x][y] < dist:
+                continue
+            for i in range(4):
+                nx = x + movement[i][0]; ny = y + movement[i][-1]
+                if -1 < nx < n and -1 < ny < n and dist + graph[nx][ny] < distances[nx][ny]:
+                    distances[nx][ny] = dist + graph[nx][ny]
+                    heapq.heappush(q, (distances[nx][ny], nx, ny))
+    dijkstra(0,0)
+    print(f'Problem {i}: {distances[n-1][n-1]}')
+    i += 1
