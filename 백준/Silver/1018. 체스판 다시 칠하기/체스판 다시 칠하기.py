837e69f9f4653257329ec16f4aeb5efa9edafa12
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-07-21 12:52:43 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,27 +1,28 @@
 import sys
 input = sys.stdin.readline
 
+def check(x,y):
+    bs = 0; ws = 0
+    for i in range(x, x+8):
+        for j in range(y, y+8):
+            if (i+j) % 2 == 0:
+                if graph[i][j] != 'B':
+                    bs += 1
+                if graph[i][j] != 'W':
+                    ws += 1
+            else:
+                if graph[i][j] != 'W':
+                    bs += 1
+                if graph[i][j] != 'B':
+                    ws += 1
+    return min(bs, ws)
+
 n, m = map(int, input().split())
 graph = [list(input().rstrip()) for _ in range(n)]
 
-res = 100**2
+res = 64
 for i in range(n-7):
     for j in range(m-7):
-        w_case = 0; b_case = 0
-        for x in range(i,i+8):
-            for y in range(j,j+8):
-                if (x+y) % 2 == 0:
-                    if graph[x][y] != 'W':
-                        w_case += 1
-                    if graph[x][y] != 'B':
-                        b_case += 1
-                else:
-                    if graph[x][y] != 'B':
-                        w_case += 1
-                    if graph[x][y] != 'W':
-                        b_case += 1
-        tmp = min(w_case, b_case)
-        if res > tmp:
-            res = tmp
+        res = min(check(i,j), res)
 
 print(res)
