1ce9723bd91e40851b5c73b1c70df33d074a5301
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-08-01 17:37:10 +0900

    [Silver II] Title: 섬의 개수, Time: 232 ms, Memory: 139464 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(x, y):
+    if x<0 or x>=h or y<0 or y>=w:
+        return
+    if graph[x][y] == 0:
+        return
+    graph[x][y] = 0
+    dfs(x+1,y)
+    dfs(x-1,y)
+    dfs(x,y+1)
+    dfs(x,y-1)
+    dfs(x-1,y-1)
+    dfs(x-1,y+1)
+    dfs(x+1,y-1)
+    dfs(x+1,y+1)
+
+while 1:
+    w, h = map(int, input().split())
+    if w == h == 0:
+        break
+    graph = [list(map(int, input().split())) for _ in range(h)]
+    
+    land = 0
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == 1:
+                land += 1
+                dfs(i, j)
+
+    print(land)
