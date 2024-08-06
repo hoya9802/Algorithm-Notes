13f51203edd88c2f7bf241872b6fa7ee286c7f96
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-08-06 19:31:19 +0900

    [Gold III] Title: 파티, Time: 64 ms, Memory: 34236 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,36 +1,40 @@
 import sys, heapq
 input = sys.stdin.readline
 INF = int(1e9)
-n, m, t = map(int, input().split())
 
-graph = [[] for _ in range(n+1)]
+n, m, x = map(int, input().split())
+
 to_graph = [[] for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
+
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a].append((b,c))
     to_graph[b].append((a,c))
 
-def dijkstra(v,gh):
+def dijkstra(x,v):
     q = []
-    distance = [INF] * (n+1)
-    distance[v] = 0
-    heapq.heappush(q, (0, v))
+    distances = [INF] * (n+1)
+    distances[x] = 0
+    heapq.heappush(q, (x,0))
+    
     while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
+        now, dist = heapq.heappop(q)
+        if distances[now] < dist:
             continue
-        for i in gh[now]:
+        for i in v[now]:
             cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-    return distance[1:]
-from_t = dijkstra(t, graph)
-to_t = dijkstra(t, to_graph)
+            if cost < distances[i[0]]:
+                distances[i[0]] = cost
+                heapq.heappush(q, (i[0], cost))
+    
+    return distances[1:]
+
+lst = dijkstra(x, graph); to_lst = dijkstra(x, to_graph)
 
 res = 0
-for a, b in zip(from_t, to_t):
-    tmp = a + b
-    if tmp > res:
-        res = tmp
+for a, b in zip(lst, to_lst):
+    if res < a+b:
+        res = a+b
+
 print(res)
