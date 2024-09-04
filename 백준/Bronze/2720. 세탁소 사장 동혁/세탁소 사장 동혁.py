c62329cbf1cb6ae8009c696272dfc89b4f2525e2
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-09-04 11:14:47 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    c = int(input())
+    res = [0] * 4
+    for idx, i in enumerate([25, 10, 5]):
+        res[idx] = c // i
+        c %= i
+    res[-1] = c
+    print(*res)
