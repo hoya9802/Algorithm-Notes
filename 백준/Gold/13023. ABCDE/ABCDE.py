40149bbc9d062751890434457f3778f89e780720
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-09-10 18:21:23 +0900

    [Gold V] Title: ABCDE, Time: 616 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -3,23 +3,27 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 graph = [[] for _ in range(n)]
+visited = [False] * (n)
 
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b); graph[b].append(a)
 
-def dfs(lst):
-    global res
-    if len(lst) == 5:
+def dfs(s, depth):
+    visited[s] = True
+
+    if depth == 5:
         print(1)
         exit()
         return
     
-    for i in graph[lst[-1]]:
-        if i not in lst:
-            dfs(lst+[i])
+    for i in graph[s]:
+        if not visited[i]:
+            dfs(i, depth+1)
+            visited[i] = False
 
 for i in range(n):
-    dfs([i])
+    dfs(i, 1)
+    visited[i] = False
 
 print(0)
