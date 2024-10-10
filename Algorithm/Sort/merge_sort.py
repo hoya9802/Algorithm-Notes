13b213a349997e8c631f54622a219ace7e4c3ba0
hoya9802 <hoya9802@gmail.com>
2024-10-10 16:06:39 +0900

    add merge sort

---
+++ Algorithm/Sort/merge_sort.py
@@ -0,0 +1,36 @@
+def merge_sort(arr, s, e):
+    if s < e:
+        mid = (s+e) // 2
+        merge_sort(arr, s, mid)
+        merge_sort(arr, mid + 1, e)
+        merge(arr, s, mid, e)
+
+def merge(arr, s, mid, e):
+    i = s; j = mid + 1
+    tmp = []
+
+    while i <= mid and j <= e:
+        if arr[i] <= arr[j]:
+            tmp.append(arr[i])
+            i += 1
+        else:
+            tmp.append(arr[j])
+            j += 1
+    
+    while i <= mid:
+        tmp.append(arr[i])
+        i += 1
+
+    while j <= e:
+        tmp.append(arr[j])
+        j += 1
+    
+    i = s; t = 0
+    while i <= e:
+        arr[i] = tmp[t]
+        i += 1; t += 1
+
+lst = list(map(int, input().split()))
+
+merge_sort(lst, 0, len(lst)-1)
+print(*lst)
