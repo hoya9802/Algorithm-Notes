d436c1fe2552200b9ee51273526a605bf56bf23d
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-10-15 11:45:27 +0900

    [Silver III] Title: 주유소, Time: 108 ms, Memory: 46360 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -4,16 +4,12 @@ input = sys.stdin.readline
 n = int(input())
 l = tuple(map(int, input().split()))
 c = tuple(map(int, input().split()))
+
+cost = c[0]
 dp = [0] * (n)
 for i in range(1,n):
-    if i == 1:
-        dp[i] = l[0] * c[0]
-        continue
-    res = int(1e9)
-    for j in range(1,i+1):
-        temp = l[i-1] * c[j-1]
-        if res > temp:
-            res = temp
-    dp[i] = dp[i-1] + res
+    if cost >= c[i-1]:
+        cost = c[i-1]
+    dp[i] = dp[i-1] + (cost * l[i-1])
 
 print(dp[-1])
