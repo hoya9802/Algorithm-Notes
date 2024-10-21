59892d283bf53a7322c324d01e008ed9cfb63537
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-10-21 09:45:08 +0900

    [Silver IV] Title: 큐 2, Time: 2400 ms, Memory: 110564 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+res = deque([])
+n = int(input())
+for i in range(n):
+    cmd = tuple(map(str, input().rsplit()))
+
+    if len(cmd) > 1:
+        res.append(int(cmd[-1]))
+        continue
+    if cmd[0] == 'pop':
+        if not res:
+            print(-1)
+            continue
+        print(res.popleft())
+    if cmd[0] == 'size':
+        print(len(res))
+    if cmd[0] == 'empty':
+        if not res:
+            print(1)
+            continue
+        print(0)
+    if cmd[0] == 'front':
+        if not res:
+            print(-1)
+            continue
+        print(res[0])
+    if cmd[0] == 'back':
+        if not res:
+            print(-1)
+            continue
+        print(res[-1])
