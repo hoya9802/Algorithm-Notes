e14916288b82cb06365dc411e57755e4f051ea7d
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-10-29 11:28:26 +0900

    [Silver V] Title: 다리 놓기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,16 +1,16 @@
 import sys
 input = sys.stdin.readline
 
+def pm(x, n):
+    total = 1
+    for i in range(x, x-n, -1):
+        total *= i
+    return total
+
+def cb(n, r):
+    return pm(n,r) // pm(r,r)
+
 for _ in range(int(input())):
     n, m = map(int, input().split())
-    dp = [[0] * (m+1) for _ in range(n+1)]
-    
-    for i in range(1, m+1):
-        dp[1][i] = i
 
-    for i in range(2,n+1):
-        for j in range(i, m+1):
-            for k in range(j, i-1, -1):
-                dp[i][j] += dp[i-1][k-1]
-
-    print(dp[n][m])
+    print(cb(m,n))
