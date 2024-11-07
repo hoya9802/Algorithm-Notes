26efcf74ee84d66bbfeb3528a61e15b96ff4a905
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-11-07 13:20:12 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,16 +1,17 @@
 import sys
+
 n = int(sys.stdin.readline())
 dp = [0] * (n+1)
-if n ==0:
-    print(0)
-    exit()
-
 def fibo(x):
     if x in (1,2):
+        dp[x] = 1
         return 1
     if dp[x] != 0:
         return dp[x]
     dp[x] = fibo(x-1) + fibo(x-2)
     return dp[x]
 
-print(fibo(n))
+if n == 0:
+    print(0)
+else:
+    print(fibo(n))
