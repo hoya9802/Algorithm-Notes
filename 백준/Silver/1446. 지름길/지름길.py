c77b049e36c7019253a08ecbe4a6900fa4103337
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-12-29 19:35:20 +0900

    [Silver I] Title: 지름길, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,42 +1,23 @@
-# 5 150
-# 0 50 10
-# 0 50 20
-# 50 100 10
-# 100 151 10
-# 110 140 90
-
-import sys, heapq
+import sys
 input = sys.stdin.readline
-INF = int(1e9)
 
 n, d = map(int, input().split())
 
-distances = [INF] * (d+1)
-graph = [[] for _ in range(d+1)]
-
-for i in range(d):
-    graph[i].append((i+1, 1))
+road = [[] for _ in range(d+1)]
 
 for _ in range(n):
     a, b, c = map(int, input().split())
-    if b <= d:
-        graph[a].append((b,c))
+    if a > d or b > d:
+        continue
+    road[b].append((a,c))
 
-def dijkstra(s):
-    distances[s] = 0
-    q = []
-    heapq.heappush(q, (0,s))
+dp = [0] * (d+1)
 
-    while q:
-        dist, now = heapq.heappop(q)
-        if distances[now] < dist:
-            continue
-        
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distances[i[0]]:
-                distances[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+for i in range(1,d+1):
+    dp[i] = dp[i-1] + 1
 
-dijkstra(0)
-print(distances[-1])
+    if road[i]:
+        for s in road[i]:
+            dp[i] = min(dp[i], dp[s[0]]+s[1])
+
+print(dp[-1])
