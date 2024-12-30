ab33e1c48d24ba133ef807a2cb401bc47c2e7ff0
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-12-30 12:40:27 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1412 ms, Memory: 101836 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,11 +1,11 @@
-import sys
-import heapq
-
+import sys, heapq
 input = sys.stdin.readline
-N, M, K, x = map(int, input().split())
+
 INF = int(1e9)
-distance = [INF] * (N+1)
+
+N, M, K, X = map(int, input().split())
 graph = [[] for _ in range(N+1)]
+distance = [INF] * (N+1)
 
 for _ in range(M):
     a, b = map(int, input().split())
@@ -26,10 +26,8 @@ def dijkstra(start):
             if cost < distance[i]:
                 distance[i] = cost
                 heapq.heappush(q, (cost, i))
-dijkstra(x)
-res = [x+1 for x in range(len(distance[1:])) if distance[x+1] == K]
-if not res:
-    print(-1)
-else:
-    for i in res:
-        print(i)
+
+dijkstra(X)
+
+res = [str(i+1) for i in range(N) if distance[i+1] == K]
+print('\n'.join(res) if res else -1)
