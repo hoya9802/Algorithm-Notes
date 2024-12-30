58d93f55939c4fc02d4bdef895a0e4017cda9489
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2024-12-30 16:42:31 +0900

    [Silver I] Title: 지름길, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,23 +1,23 @@
+# dp solution
 import sys
 input = sys.stdin.readline
 
 n, d = map(int, input().split())
 
-road = [[] for _ in range(d+1)]
+short = [[] for _ in range(d+1)]
 
 for _ in range(n):
     a, b, c = map(int, input().split())
     if a > d or b > d:
         continue
-    road[b].append((a,c))
+    short[b].append((a,c))
 
 dp = [0] * (d+1)
 
-for i in range(1,d+1):
+for i in range(1, d+1):
     dp[i] = dp[i-1] + 1
-
-    if road[i]:
-        for s in road[i]:
-            dp[i] = min(dp[i], dp[s[0]]+s[1])
+    if short[i]:
+        for j in short[i]:
+            dp[i] = min(dp[i], dp[j[0]] + j[1])
 
 print(dp[-1])
