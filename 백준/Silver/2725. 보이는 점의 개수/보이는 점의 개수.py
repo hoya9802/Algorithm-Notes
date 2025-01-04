fb5a611ca3ac97584ef22986427895939cea79aa
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-01-04 21:27:01 +0900

    [Silver II] Title: 보이는 점의 개수, Time: 268 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2725. 보이는 점의 개수/보이는 점의 개수.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+tc = [int(input()) for _ in range(int(input()))]
+max_val = max(tc)
+
+def gcd(n, m):
+    if m == 0:
+        return n
+    return gcd(m, n%m)
+
+dp = [0] * (max_val+1)
+dp[1] = 3
+
+for i in range(2, max_val+1):
+    cnt = 0
+    for j in range(1, i):
+        if gcd(i,j) == 1:
+            cnt += 2
+    dp[i] = dp[i-1] + cnt
+
+print('\n'.join(map(str, [dp[i] for i in tc])))
