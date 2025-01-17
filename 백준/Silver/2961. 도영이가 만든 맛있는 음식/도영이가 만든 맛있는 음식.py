9a9d58ada8c7de181ec3f629bb2a2f72ea423a77
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-01-17 19:43:26 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -2,26 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-ingred = [list(map(int, input().split())) for _ in range(n)]
+ingredients = [list(map(int, input().split())) for _ in range(n)]
 
-res = float('inf')
-
-def dfs(idx, current_s, current_b, count):
+res = int(1e9)
+def dfs(idx, s, b, count):
     global res
-    
-    # 종료 조건: 모든 재료를 탐색했을 때
+
     if idx == n:
-        # 하나 이상의 재료를 사용한 경우에만 계산
         if count > 0:
-            res = min(res, abs(current_s - current_b))
+            res = min(res, abs(s-b))
         return
 
-    # 1. 현재 재료를 선택한 경우
-    dfs(idx + 1, current_s * ingred[idx][0], current_b + ingred[idx][1], count + 1)
+    dfs(idx+1, s*ingredients[idx][0], b+ingredients[idx][1], count+1)
 
-    # 2. 현재 재료를 선택하지 않은 경우
-    dfs(idx + 1, current_s, current_b, count)
+    dfs(idx+1, s, b, count)
 
-# 초기 상태: 신맛=1, 쓴맛=0, 선택한 재료 수=0
 dfs(0, 1, 0, 0)
 print(res)
