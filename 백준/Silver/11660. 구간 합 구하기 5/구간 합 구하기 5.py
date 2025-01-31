2d3ded56d0cda84e8faf4b47823d27dc583d2b6f
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-01-31 20:08:07 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 960 ms, Memory: 73388 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,16 +1,17 @@
 import sys
-input = sys.stdin.readline
+input =  sys.stdin.readline
 
 n, m = map(int, input().split())
-graph = []
-graph.append([0]*(n+1))
-for _ in range(n):
-    graph.append([0]+list(map(int, input().split())))
 
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        graph[i][j] += graph[i][j-1] + graph[i-1][j] - graph[i-1][j-1]
+arr = [[0] * (n+1)]
+for _ in range(n):
+    arr.append([0]+list(map(int, input().split())))
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        arr[i][j] += arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1]
 
 for _ in range(m):
-    sx, sy, ex, ey = map(int, input().split())
-    print(graph[ex][ey] - graph[ex][sy-1] - graph[sx-1][ey] + graph[sx-1][sy-1])
+    x1, y1, x2, y2 = map(int, input().split())
+
+    print(arr[x2][y2] - arr[x2][y1-1] - arr[x1-1][y2] + arr[x1-1][y1-1])
