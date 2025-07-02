4c6d6d7a5e0732a3acabb49a03113310f0ea4d51
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-07-02 17:06:25 +0900

    [Silver V] Title: 날짜 계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.py
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -2,19 +2,10 @@ import sys
 input = sys.stdin.readline
 
 e, s, m = map(int, input().split())
-x, y, z = 0, 0, 0
-year = 0
+year = 1
 
 while 1:
-    x += 1; y += 1; z += 1
-    year += 1
-    if x > 15:
-        x = 1
-    if y > 28:
-        y = 1
-    if z > 19:
-        z = 1
-    
-    if (e, s, m) == (x, y, z):
+    if (year - e) % 15 + (year - s) % 28 + (year - m) % 19 == 0:
         print(year)
         break
+    year += 1
