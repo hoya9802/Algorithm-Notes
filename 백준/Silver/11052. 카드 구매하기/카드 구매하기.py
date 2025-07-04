9b410473a66177a06df664937a89d8e6e14fcdea
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-07-04 21:53:11 +0900

    [Silver I] Title: 카드 구매하기, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -2,18 +2,12 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-p = list(map(int, input().split()))
-lp = len(p)
+p = [0] + list(map(int, input().split()))
 
-dp = [0] * (n+lp)
+for i in range(1, n+1):
+    for j in range(i, n-i+1):
+        tmp = p[i] + p[j]
+        if p[i+j] < tmp:
+            p[i+j] = tmp
 
-for i in range(len(p), len(dp)):
-    tmp=[]
-
-    for j in range(lp):
-        if (i-(lp-1)) >= j+1:
-            v = dp[i-(j+1)] + p[j]
-            tmp.append(v)
-    dp[i] = max(tmp)
-
-print(dp[-1])
+print(p[-1])
