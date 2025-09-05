0f93ed0a9306d141b831faac3853a3e5428e4eda
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-09-06 00:56:57 +0900

    [level 3] Title: 스티커 모으기(2), Time: 63.44 ms, Memory: 15.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -0,0 +1,17 @@
+def solution(sticker):
+    l = len(sticker)
+    if l == 1:
+        return sticker[0]
+    dp1 = [0] * l
+    dp1[0] = sticker[0]
+    dp1[1] = sticker[0]
+    for i in range(2, l-1):
+        dp1[i] = max(dp1[i-1], dp1[i-2]+sticker[i])
+    
+    dp2 = [0] * l
+    dp2[0] = 0
+    dp2[1] = sticker[1]
+    for i in range(2, l):
+        dp2[i] = max(dp2[i-1], dp2[i-2]+sticker[i])
+    
+    return max(dp1[-2], dp2[-1])
