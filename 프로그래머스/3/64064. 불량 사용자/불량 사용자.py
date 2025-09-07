16e7c2c87dcacfcc32058a2fb1cd56b3de6aaba7
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-09-07 16:10:24 +0900

    [level 3] Title: 불량 사용자, Time: 250.24 ms, Memory: 40.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,42 @@
+def checker(user_id, banned_ids, banned_visited):
+    for bi in range(len(banned_ids)):
+        if banned_visited[bi] == 1:
+            continue
+        if len(user_id) != len(banned_ids[bi]):
+            continue
+        else:
+            for u, b in zip(user_id, banned_ids[bi]):
+                if b=="*" or u==b:
+                    continue
+                else:
+                    break
+            else:
+                return bi    
+    return 100
+
+def solution(user_id, banned_id):
+    ans=0
+    user_visited = [0] * len(user_id)
+    banned_visited = [0] * len(banned_id)
+    
+    tmp = []
+    def dfs(user_id, banned_id, user_visited, banned_visited, arr):
+        nonlocal tmp
+        if len(arr) == len(banned_id):
+            tmp.append(set(arr))
+            return
+        for i in range(len(user_id)):
+            if user_visited[i] == 0:
+                res = checker(user_id[i], banned_id, banned_visited)
+                if res > 8:
+                    continue
+                user_visited[i] = 1
+                banned_visited[res] = 1
+                dfs(user_id, banned_id, user_visited, banned_visited, arr+[user_id[i]])
+                user_visited[i] = 0
+                banned_visited[res] = 0
+                
+    dfs(user_id, banned_id, user_visited, banned_visited, [])
+    
+    ans = len(set(map(frozenset, tmp)))
+    return ans
