4372d9a6999e76ec56c484267a019efd6bb0b896
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-09-12 18:31:52 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 58.76 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,28 +1,24 @@
 from collections import defaultdict
 
 def solution(gems):
-    gem_types = len(set(gems))
-    n = len(gems)
-    
-    answer = [0, n - 1]
-    counter = defaultdict(int)
-    
+    gem_type = len(set(gems))
+    gems_size = len(gems)
     start, end = 0, 0
-    counter[gems[0]] = 1
-    
-    while start < n and end < n:
-        if len(counter) == gem_types:
+    ans = [0, gems_size-1]
+    gem_dict = defaultdict(int)
+    gem_dict[gems[0]] += 1
 
-            if (end - start) < (answer[1] - answer[0]):
-                answer = [start, end]
-            
-            counter[gems[start]] -= 1
-            if counter[gems[start]] == 0:
-                del counter[gems[start]]
+    while start < gems_size and end < gems_size:
+        if len(gem_dict) == gem_type:
+            if (end - start) < (ans[-1] - ans[0]):
+                ans = [start, end]
+            gem_dict[gems[start]] -= 1
+            if gem_dict[gems[start]] == 0:
+                del gem_dict[gems[start]]
             start += 1
         else:
             end += 1
-            if end < n:
-                counter[gems[end]] += 1
+            if end < gems_size:
+                gem_dict[gems[end]] += 1
     
-    return [answer[0] + 1, answer[1] + 1]
+    return [ans[0]+1, ans[-1]+1]
