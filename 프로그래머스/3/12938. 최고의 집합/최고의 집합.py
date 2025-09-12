3f0aeb75b125d5f1f6400ec05f51318fbb145b3d
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-09-13 03:03:03 +0900

    [level 3] Title: 최고의 집합, Time: 0.67 ms, Memory: 9.71 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -1,12 +1,13 @@
 def solution(n, s):
     if n > s:
         return [-1]
-    ans = [s//n] * n
+    ans = [int(s//n)] * n
     left = s%n
-    
-    p = len(ans)-1
-    while left > 0:
-        ans[p] += 1
+    for i in range(len(ans)-1, -1, -1):
+        if left == 0:
+            break
+        ans[i] += 1
         left -= 1
-        p -= 1
     return ans
+        
+    
