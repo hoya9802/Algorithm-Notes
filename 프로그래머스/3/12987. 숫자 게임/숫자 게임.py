9faf645f2e49258dd411a5ce1a3217c33b69c38d
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-09-13 01:31:01 +0900

    [level 3] Title: 숫자 게임, Time: 46.54 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -1,16 +1,13 @@
 def solution(A, B):
-    A.sort()
-    B.sort()
-    
-    result=0; point=0;
-    
-    for a in A:
-        while point < len(A) and B[point] <= a:
-            point += 1
-        if point < len(A):
-            result += 1
-            point += 1
+    n = len(A)
+    A.sort(reverse=True)
+    B.sort(reverse=True)
+    ans = 0
+    i, j = 0, 0
+    while i < n and j < n:
+        if A[i] < B[j]:
+            ans += 1
+            i += 1; j += 1
         else:
-            break
-    
-    return result
+            i += 1
+    return ans
