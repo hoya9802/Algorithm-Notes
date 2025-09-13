2627c4f90da746307b5a1455c5010ac4ca9c11a6
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-09-13 18:09:35 +0900

    [level 3] Title: 베스트앨범, Time: 0.10 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,19 +1,18 @@
 def solution(genres, plays):
-    data = {}
-
+    ans = {}; res = []
     for i in range(len(genres)):
-        if not genres[i] in data:
-            data[genres[i]] = {'t':0, 'l':[]}
-        data[genres[i]]['l'].append({'v': plays[i],'id': i})
-        data[genres[i]]['t'] += plays[i]
-
-    data = list(data.values())
-    data.sort(key=lambda x: x['t'], reverse=True)
-
-    ans = []
-    for i in range(len(data)):
-        tmp = sorted(data[i]['l'], key=lambda x:x['v'], reverse=True)
+        if genres[i] in ans:
+            ans[genres[i]]['t'] += plays[i]
+            ans[genres[i]]['l'].append((plays[i], i))
+            continue
+        ans[genres[i]] = {'t':plays[i], 'l':[(plays[i], i)]}
+    
+    ans = list(ans.values())
+    ans.sort(key=lambda x:x['t'], reverse=True)
+    
+    for i in ans:
+        tmp = sorted(i['l'], key=lambda x: x[0], reverse=True)
         for j in range(2 if len(tmp) >= 2 else len(tmp)):
-            ans.append(tmp[j]['id'])
-
-    return ans
+            res.append(tmp[j][-1])
+            
+    return res
