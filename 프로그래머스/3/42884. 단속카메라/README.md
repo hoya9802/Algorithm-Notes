12a0367db65538e9f0ba439d49b72fd0b7e1b9a9
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-09-13 16:30:49 +0900

    [level 3] Title: 단속카메라, Time: 1.37 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/README.md
+++ 프로그래머스/3/42884. 단속카메라/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단속카메라 - 42884 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42884) 
 
 ### 성능 요약
 
-메모리: 9.59 MB, 시간: 0.94 ms
+메모리: 9.54 MB, 시간: 1.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 23일 14:47:27
+2025년 09월 13일 16:30:46
 
 ### 문제 설명
 
--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,10 +1,12 @@
 def solution(routes):
     routes.sort(key=lambda x: x[-1])
-    new_camera = routes[0][-1]
-    ans = 1
-    for i in range(1, len(routes)):
-        if new_camera < routes[i][0]:
-            new_camera = routes[i][-1]
-            ans+=1
+    ans = 0; cur = 0; pointer = 0
+    
+    while pointer < len(routes):
+        if routes[cur][-1] >= routes[pointer][0]:
+            pointer += 1
+        else:
+            cur = pointer
+            ans += 1
 
-    return ans
+    return ans+1
