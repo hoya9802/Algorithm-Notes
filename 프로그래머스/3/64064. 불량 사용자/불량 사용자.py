ebfb885ef3e45cc1084ab4817898721b9e69333b
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-09-14 15:04:09 +0900

    [level 3] Title: 불량 사용자, Time: 156.12 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,40 +1,21 @@
-def checker(user_id, banned_ids, banned_visited):
-    for bi in range(len(banned_ids)):
-        if banned_visited[bi] == 1 or len(user_id) != len(banned_ids[bi]):
-            continue
-        else:
-            for u, b in zip(user_id, banned_ids[bi]):
-                if b=="*" or u==b:
-                    continue
-                else:
-                    break
-            else:
-                return bi    
-    return 100
+def matches(user, ban):
+    if len(user) != len(ban):
+        return False
+    for u, b in zip(user, ban):
+        if b != '*' and u != b:
+            return False
+    return True
 
 def solution(user_id, banned_id):
-    ans=0
-    user_visited = [0] * len(user_id)
-    banned_visited = [0] * len(banned_id)
-    
-    tmp = []
-    def dfs(user_id, banned_id, user_visited, banned_visited, arr):
-        nonlocal tmp
-        if len(arr) == len(banned_id):
-            tmp.append(set(arr))
+    ans = set()
+
+    def dfs(idx, chosen):
+        if idx == len(banned_id):
+            ans.add(frozenset(chosen))
             return
-        for i in range(len(user_id)):
-            if user_visited[i] == 0:
-                res = checker(user_id[i], banned_id, banned_visited)
-                if res > 8:
-                    continue
-                user_visited[i] = 1
-                banned_visited[res] = 1
-                dfs(user_id, banned_id, user_visited, banned_visited, arr+[user_id[i]])
-                user_visited[i] = 0
-                banned_visited[res] = 0
-                
-    dfs(user_id, banned_id, user_visited, banned_visited, [])
-    
-    ans = len(set(map(frozenset, tmp)))
-    return ans
+        for u in user_id:
+            if u not in chosen and matches(u, banned_id[idx]):
+                dfs(idx + 1, chosen + [u])
+
+    dfs(0, [])
+    return len(ans)
