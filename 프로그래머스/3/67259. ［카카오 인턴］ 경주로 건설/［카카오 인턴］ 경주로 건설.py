1b74f6dd4abcea5850a46583a933506dac5f5a1f
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-09-30 17:27:07 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 30.71 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def solution(board):
+    n = len(board)
+    INF = int(1e9)
+    movement = [(0,-1), (1,0), (0,1), (-1,0)]
+    
+    visited = [[[INF]*4 for _ in range(n)] for _ in range(n)]
+    q = deque([])
+    
+
+    for d in range(4):
+        visited[0][0][d] = 0
+        q.append((0, 0, d, 0))  # (x, y, dir, cost)
+    
+    res = INF
+    while q:
+        x, y, d, cost = q.popleft()
+        
+        if (x, y) == (n-1, n-1):
+            res = min(res, cost)
+            continue
+        
+        for nd, (mx, my) in enumerate(movement):
+            nx, ny = x + mx, y + my
+            if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:
+                # 방향 유지 = 100, 회전 = 600
+                new_cost = cost + (100 if d == nd else 600)
+                
+                if visited[nx][ny][nd] > new_cost:
+                    visited[nx][ny][nd] = new_cost
+                    q.append((nx, ny, nd, new_cost))
+    
+    return res
