3a40129677127f5d50b861189d5fa4b39de3fb21
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-10-05 14:48:35 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.49 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,22 +1,22 @@
 import heapq
 
-class Job():
+class Job:
     def __init__(self, time=0, cost=0):
         self.time = time
         self.cost = cost
         
     def __lt__(self, other):
         return self.cost < other.cost
-    
+        
     def __le__(self, other):
         return self.cost <= other.cost
 
 def solution(jobs):
     jobs.sort()
     
-    next_idx = 1
     job_queue = [Job(jobs[0][0], jobs[0][1])]
     now = jobs[0][0]
+    next_idx = 1
     ans = 0
     
     while 1:
@@ -24,18 +24,19 @@ def solution(jobs):
             job = Job(jobs[next_idx][0], jobs[next_idx][1])
             heapq.heappush(job_queue, job)
             next_idx += 1
-        
+            
         if len(job_queue) == 0:
             if next_idx < len(jobs):
                 job = Job(jobs[next_idx][0], jobs[next_idx][1])
                 heapq.heappush(job_queue, job)
                 now = job.time
                 next_idx += 1
+            
             else:
                 break
-                
+        
         next_job = heapq.heappop(job_queue)
         now += next_job.cost
-        ans += (now - next_job.time)
-    
+        ans += now - next_job.time
+        
     return ans // len(jobs)
