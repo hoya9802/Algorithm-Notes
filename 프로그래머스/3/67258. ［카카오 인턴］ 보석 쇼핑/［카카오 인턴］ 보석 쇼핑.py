f6ed633189b5cfc457fd49f01dd91475c6df2e17
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-10-07 16:24:26 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 56.42 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,19 +1,19 @@
 from collections import defaultdict
 
 def solution(gems):
-    gem_type = set(gems)
     n = len(gems)
-    ans = [0, n-1]
+    gem_type = len(set(gems))
+    ans = [0, n]
     start, end = 0, 0
     gem_dict = defaultdict(int)
-    gem_dict[gems[start]] = 1
+    gem_dict[gems[end]] += 1
     
     while start < n and end < n:
-        if len(gem_dict) == len(gem_type):
-            if (end - start) < (ans[-1] - ans[0]):
+        if len(gem_dict) == gem_type:
+            if (end-start) < (ans[-1]-ans[0]):
                 ans = [start, end]
             gem_dict[gems[start]] -= 1
-            if gem_dict[gems[start]] == 0:
+            if gem_dict[gems[start]] <= 0:
                 del gem_dict[gems[start]]
             start += 1
         else:
