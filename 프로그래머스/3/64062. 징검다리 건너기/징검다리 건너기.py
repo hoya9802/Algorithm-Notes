ddfe7b3c96d8f98be04f017fef473d1b41ead3e6
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-10-08 13:29:36 +0900

    [level 3] Title: 징검다리 건너기, Time: 302.54 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,25 +1,32 @@
-def canCross(stones, k, people):
+def checker(stones, k, people):
     count = 0
-    for stone in stones:
-        if stone < people:
+    for s in stones:
+        if s < people:
             count += 1
-            if count >= k:
+            if count == k:
                 return False
         else:
             count = 0
-    return True
+    else:
+        return True
+            
+def binary_search(s, e, k, stones):
+    global ans
+    if e < s:
+        return
+    mid = (s + e) // 2
+    if checker(stones, k, mid):
+        if ans < mid:
+            ans = mid
+        binary_search(mid+1, e, k, stones)
+    else:
+        binary_search(s, mid-1, k, stones)
 
-
+ans = 0
+        
 def solution(stones, k):
-    left, right = 1, max(stones)
-    answer = 0
-
-    while left <= right:
-        mid = (left + right) // 2
-        if canCross(stones, k, mid):
-            answer = mid
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return answer
+    start = 0; end = max(stones)
+    
+    binary_search(start, end, k, stones)
+    
+    return ans
