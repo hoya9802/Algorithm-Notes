1cdfd41b37a319a11aa126ef89d52da3260fd303
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-10-08 14:43:41 +0900

    [level 3] Title: 가장 먼 노드, Time: 7752.58 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -3,37 +3,26 @@ import heapq
 def solution(n, edge):
     INF = int(1e9)
     graph = [[] for _ in range(n+1)]
-    visited = [0] * (n+1)
-    distances = [INF] * (n+1)
-    
+    distance = [INF] * (n+1)
     for e in edge:
-        a, b = e
-        graph[a].append(b)
-        graph[b].append(a)
-
+        graph[e[0]].append(e[1])
+        graph[e[1]].append(e[0])
+    
     def dijkstra(v):
-        nonlocal visited, distances
+        distance[v] = 0
         q = []
         heapq.heappush(q, (0, v))
         
         while q:
             dist, now = heapq.heappop(q)
-            distances[now] = dist
-            visited[now] = 1
-            
+            if dist > distance[now]:
+                continue
             for i in graph[now]:
-                if dist + 1 < distances[i]:
-                    distances[i] = dist + 1
-                    
-                    heapq.heappush(q, (distances[i],i))
+                cost = dist + 1
+                if cost < distance[i]:
+                    distance[i] = cost
+                    heapq.heappush(q, (cost, i))
         
     dijkstra(1)
     
-    max_val = 0
-    for d in range(1, len(distances)):
-        if distances[d] == INF:
-            continue
-        if distances[d] > max_val:
-            max_val = distances[d]
-    
-    return sum([1 for i in range(1, len(distances)) if distances[i] == max_val])
+    return sum([1 for i in distance[1:] if i == max(distance[1:])])
