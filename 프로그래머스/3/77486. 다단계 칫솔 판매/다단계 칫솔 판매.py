c241c1c7a619cc3129086dde5e79eac8c7914ac9
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-10-17 01:29:34 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 174.97 ms, Memory: 20.7 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -2,28 +2,28 @@ class Node:
     def __init__(self, parent):
         self.total = 0
         self.parent = parent
+
+    def divide(self, earn_money):
+        tmp = int(earn_money * 0.1)
+        if tmp > 0 and self.parent != None:
+            self.parent.divide(tmp)
+        self.total += (earn_money-tmp)
         
-    def divider(self, money):
-        div = int(money * .1)
-        self.total += (money - div)
-        if div > 0 and self.parent:
-            self.parent.divider(div)
 
 def solution(enroll, referral, seller, amount):
-    N = len(enroll)
-    rel_dict = {'-':None}
-    ans = []
     
-    for i in range(N):
-        person = enroll[i]; parent = referral[i]
-        if parent == '-':
-            node = Node(rel_dict['-'])
-        else:    
-            node = Node(rel_dict[parent])
-        rel_dict.update({person: node})
-
-    for i in range(len(seller)):
-        cost = amount[i] * 100
-        rel_dict[seller[i]].divider(cost)
+    tree = {}
+    tree['-'] = None
     
-    return [rel_dict[enroll[i]].total for i in range(N)]
+    for e, r in zip(enroll, referral):
+        if r == "-":
+            tree[e] = Node(tree['-'])
+            continue
+        tree[e] = Node(tree[r])
+    
+    for s, a in zip(seller, amount):
+        earn_money = a * 100
+        tree[s].divide(earn_money)
+    
+    return [tree[x].total for x in enroll]
+    
