44322dc29925289e6ba10d758fbb6cd58f9bed86
EUNTAEK <73452270+hoya9802@users.noreply.github.com>
2025-10-18 22:47:31 +0900

    [level 3] Title: 입국심사, Time: 479.67 ms, Memory: 13.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,24 +1,27 @@
+def checker(n, times, time):
+    total = 0
+    
+    for t in times:
+        total += (time//t)
+        if total >= n:
+            return True
+    return False
+
 def solution(n, times):
-    res = min(times) * n
+    min_time = min(times) * n
     
     def binary_search(s, e):
-        nonlocal res
+        nonlocal min_time
         if s > e:
             return
         mid = (s+e)//2
-        p=0
-        for t in times:
-            p += mid // t
-            
-            if p >= n:
-                if res > mid:
-                    res = mid
-                binary_search(s, mid-1)
-                break
-                
+        
+        if checker(n, times, mid):
+            min_time = mid
+            binary_search(s, mid-1)
         else:
             binary_search(mid+1, e)
     
-    binary_search(0, res)
+    binary_search(1, min_time)
     
-    return res
+    return min_time
